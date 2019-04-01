627903dd2544ac27ef977594653ce59562e51ffa
Carlos Garcia <carlos.garcia@nb-cgarcia.local>
2019-04-01 11:58:02 +0200

    Start Websockets JS

--- lib/alexio_web/channels/user_socket.ex
+++ lib/alexio_web/channels/user_socket.ex
@@ -2,7 +2,7 @@ defmodule AlexioWeb.UserSocket do
   use Phoenix.Socket
 
   ## Channels
-  # channel "room:*", AlexioWeb.RoomChannel
+  channel "room:*", AlexioWeb.RoomChannel
 
   # Socket params are passed from the client and can
   # be used to verify and authenticate a user. After
