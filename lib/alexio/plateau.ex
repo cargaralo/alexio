af53fe8a48b81becbfa302bcf759ae173979d47a
Carlos Garcia <carlos.garcia@nb-cgarcia.local>
2019-04-01 18:08:45 +0200

    Random positions

--- lib/alexio/plateau.ex
+++ lib/alexio/plateau.ex
@@ -10,10 +10,11 @@ defmodule Alexio.Plateau do
     if player_name_owned?(plateau, player_name) do
       {:error, plateau}
     else
-      new_player = Alexio.Player.init(player_name, 0, 0)
+      {generated_x, generated_y} = empty_position(plateau)
+      new_player = Alexio.Player.init(player_name, generated_x, generated_y)
 
       players = Map.put(plateau.players, new_player.name, new_player)
-      positions = Map.put(plateau.positions, {new_player.x_position, new_player.y_position}, player_name)
+      positions = Map.put(plateau.positions, {generated_x, generated_y}, player_name)
 
       {:ok, %__MODULE__{plateau | players: players, positions: positions}}
     end
@@ -21,5 +22,15 @@ defmodule Alexio.Plateau do
 
   def player_name_owned?(plateau = %__MODULE__{}, player_name) do
     Map.has_key?(plateau.players, player_name)
+  end
+
+  defp empty_position(plateau = %__MODULE__{}) do
+    random_position = {Enum.random(0..plateau.x_size - 1), Enum.random(0..plateau.y_size - 1)}
+
+    if Map.has_key?(plateau.positions, random_position) do
+      empty_position(plateau)
+    else
+      random_position
+    end
   end
 end
